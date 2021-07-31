67c25dd38aa946fb5cf307a63f7baf64b30a37f8
duyvt <vuthanhduy93@gmail.com>
2021-07-31 09:43:53 +0700

    add hoisting lesson

---
+++ hoisting.js
@@ -0,0 +1,8 @@
+//ta khai báo 1 biến sử dụng var:
+
+var a =2
+console.log (a) //2
+// khi ta khai báo var a=2 sau dòng console.log(a)
+// thì sẽ trả về result là undefined vì JS thực hiện lệnh từ trên xuống và không thấy define của a trước lệnh console
+// var a =3
+//vì vậy ta sử 
