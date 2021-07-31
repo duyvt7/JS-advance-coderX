f443fc75abfef4bfd25eca3ca7f1c0c22d49e877
duyvt <vuthanhduy93@gmail.com>
2021-07-31 17:26:41 +0700

    add some example

--- defaultParameter.js
+++ defaultParameter.js
@@ -4,3 +4,14 @@ function greet(name = 'friend') {
 greet()
 //nếu không truyền tham số vào trong hàm 
 // thì hàm sẽ lấy giá trị default là friend
+
+function greet2 (name = 'friend', language= 'en'){
+    if (language === 'en'){
+        console.log(`Hi ${name}`)
+    }else if(language = 'cn'){
+        console.log(`ni hao, ${name}`)
+    }else{
+        console.log(`chao ban ${name}`)
+    }
+}
+greet2('duy')
