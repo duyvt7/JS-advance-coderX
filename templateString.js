8b5b9836ef5b1ae2306b1ffc3da65fcee45f5e29
duyvt <vuthanhduy93@gmail.com>
2021-07-31 15:58:19 +0700

    template String

---
+++ templateString.js
@@ -0,0 +1,10 @@
+function greeting (name){
+    return 'hi,'+ name+'!'
+}
+//using template String
+function greeting2 (name){
+    return `hi, ${name}!`
+}
+
+var result = greeting2('Duy')
+console.log(result)
