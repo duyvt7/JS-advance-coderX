340707331d78602681079c11c9113ccd7169033e
duyvu <vuthanhduy93@gmail.com>
2021-08-01 08:58:58 +0700

    enhance obj

---
+++ enhanceLiteralObj.js
@@ -0,0 +1,14 @@
+// create an obj 
+//using constructor function
+function Mouse (name){
+    this.name =name;
+}
+var Jerry = new Mouse('Jerry')
+
+//obj literal
+
+var Cat = {
+    name: 'Tom',
+    weight: 5,
+}
+console.log(Cat)
