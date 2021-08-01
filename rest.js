e633dbf561fbb181847106779eb373069c428947
duyvt <vuthanhduy93@gmail.com>
2021-08-01 21:09:18 +0700

    spread

--- rest.js
+++ rest.js
@@ -1,9 +1,17 @@
-function log(...a){
+function log(a,...numbers){
     console.log(a)
+    console.log(numbers)
 }
 log(1,2,3,4)
 function concat (seperator, ...strings){
     return strings.join(seperator);
 }
+function sum (...a){
+    return a.reduce((a,b) => {
+        return a+b
+    })
+}
+let b= sum(1,2,3)
+console.log (b)
 var a = concat('.','a','b','c')
 console.log (a)
---
+++ spread.js
@@ -0,0 +1,5 @@
+const a = [1,2,3]
+const b = [0,...a,4]
+console.log (b)
+//spread : trải phần tử ra
+//rest: gom phần từ lại
