d8e99c50f588b9de7e8379b18de4479fc84d30eb
duyvu <vuthanhduy93@gmail.com>
2021-08-01 11:40:36 +0700

    more example of rest

--- rest.js
+++ rest.js
@@ -2,3 +2,8 @@ function log(...a){
     console.log(a)
 }
 log(1,2,3,4)
+function concat (seperator, ...strings){
+    return strings.join(seperator);
+}
+var a = concat('.','a','b','c')
+console.log (a)
