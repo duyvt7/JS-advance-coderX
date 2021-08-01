26fe5739b0a9d4013c7663a3846b4ec42336b524
duyvu <vuthanhduy93@gmail.com>
2021-08-01 09:06:38 +0700

    add more enhance obj literal example

--- enhanceLiteralObj.js
+++ enhanceLiteralObj.js
@@ -12,3 +12,16 @@ var Cat = {
     weight: 5,
 }
 console.log(Cat)
+
+//enhance obj literal
+const name = 'Spike',
+const Dog = {
+    name,// name : name;
+    run() {
+        console.log('this dog is eatting ...')
+    }
+}
+console.log(Dog.run)
+
+//nodeJS dont assist this function, 
+//this function is assisted by ES6
